90a8b117f7b5d0cf69b2da3c41a0da9e936e4cdb
Sean <seon2you@hotmail.com>
2017-02-18 21:41:52 -0800

    added angular factory

---
+++ src/CorePatientRecords/wwwroot/template/ng-rest.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    'use strict';
+    var prApp = angular.module('patientRecordsApp', []);
+})();
+
+(function () {
+    'use strict';
+
+    var pService = 'patientFactory';
+
+    angular.module('patientRecordsApp').factory(pService, ['$http', patientFactory]);
+
+    function patientFactory($http) {
+        function getPatientsFromApi() {
+            return $http.get('http://localhost:23799/api/Patient');
+        }
+
+        var patientService = {
+            getPatientsFromApi: getPatientsFromApi
+        };
+        return patientService;
+    }
+})();
+
+(function () {
+    'use strict';
+    var pController = 'patientController';
+
+    angular.module('patientRecordsApp').controller(pController, ['$scope', 'patientFactory', patientController]);
+
+    function patientController($scope, patientFactory) {
+        $scope.patients = [];
+
+        patientFactory.getPatientsFromApi().success(function (patientData) {
+            $scope.patients = patientData;
+            console.log("Data obtained successfully.");
+        }).error(function (error) {
+            console.log("An error has occured.");
+        });
+    }
+})();
---
+++ src/CorePatientRecords/wwwroot/template/ng-scope-module.js
@@ -0,0 +1,6 @@
+﻿var patientRecordsApp = angular.module('patientRecordsApp', []);
+patientRecordsApp.controller('patientController', function ($scope) {
+    $scope.lastName = 'Smith';
+    $scope.firstName = 'John';
+    $scope.socialSecurityNumber = '123450001';
+});
